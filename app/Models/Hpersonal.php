dc500ca411e8cfd3795be3cf5348730fc96f16ef
John Israel Aquino <jbaquino3@up.edu.ph>
2023-03-20 08:26:51 +0800

    auth models

---
+++ app/Models/Hpersonal.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hpersonal extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hpersonal';
+
+    protected $primaryKey = 'employeeid';
+
+    protected $fillable = [
+        'employeeid',
+        'lastname',
+        'firstname',
+        'middlename',
+        'postitle',
+        'deptcode',
+    ];
+
+    public $timestamps = false;
+
+    public function htypser()
+    {
+        return $this->belongsTo(Htypser::class, 'deptcode', 'tscode');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'employeeid', 'employeeid');
+    }
+}
---
+++ app/Models/Htypser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Htypser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'htypser';
+
+    protected $primaryKey = 'tscode';
+
+    protected $fillable = [
+        'tscode',
+        'tsdesc',
+        'tsstat',
+        'tstype',
+    ];
+
+    public $timestamps = false;
+}
